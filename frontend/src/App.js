75ed229e93e5479561dfe32435c6a9399dd3990b
amr4356 <amrooo73321@gmail.com>
2024-07-19 14:13:35 +0300

    354. Working with action() Functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ import {RouterProvider,createBrowserRouter} from 'react-router-dom';
 import HomePage from './pages/Home';
 import EventsPage,{loader as eventsLoader} from './pages/Events';
 import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
+import NewEventPage, {action as newEventAction} from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
@@ -44,7 +44,7 @@ const router=createBrowserRouter([
         { index:true , element: <EventDetailPage />},
         { path:'edit' , element: <EditEventPage />},  
       ]},
-      { path:'new' , element: <NewEventPage />},
+      { path:'new' , element: <NewEventPage />, action: newEventAction },
     ]}
     ],
   },
